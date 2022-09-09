29e9b153794ae249841bf5afaaab8b47e094f40a
Dafarlog <Dafarlog@gmail.com>
2022-09-09 13:43:27 +0300

    add fetchCountryByName

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+function fetchCountryByName(name){
+    const baseUrl ="https://restcountries.com/v3.1/";
+    const endPoint ="name/"
+    return fetch(`${baseUrl}${endPoint}${name}`)
+    .then(resp => {
+    if(!resp.ok){
+        throw new Error();
+    }
+    return resp.json();
+    })
+    .catch(console.log);
+}
+
+console.log(fetchCountryByName("unit").then(data => console.log(data.length)))
+
